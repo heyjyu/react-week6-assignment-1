4df2a7b3dc2322b6c84085db9a999ede13638732
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-05-02 22:23:36 +0900

    Refactor
    Refactor.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -73,7 +73,7 @@ describe('App', () => {
     });
   });
 
-  context('with undefined paths', () => {
+  context('with invalid paths', () => {
     it('renders error message', () => {
       const { container } = renderApp({ path: '/otherPage' });
 
